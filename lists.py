54b60e75b7f4e01b77adcce3a47cec436f9af634
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-06-28 22:06:13 +0100

    Add files via upload

--- lists.py
+++ lists.py
@@ -1,7 +1,7 @@
 import os
 print("\033c\033[47;31m\ngive me the .java file to open ? \n")
 a=input().strip()
-a="Hello.java"
+#a="Hello.java"
 b=a.find(".")
 c=a
 if b>-1:
@@ -14,6 +14,7 @@ aa=f1.read()
 f1.close()
 bb=aa.split("\n")
 steps=0
+u="nop\n"
 for aaa in bb:
     aaa=aaa.strip()
     if aaa!="":
@@ -22,20 +23,40 @@ for aaa in bb:
         ggg=aaa.find(" static")
         gggg=aaa.find("void")
         if (g>-1 or gg>-1 or ggg>-1) and steps==0:
-            aaa=aaa.replace("private","")
-            aaa=aaa.replace("public","")
-            aaa=aaa.replace("static","")
-            aaa=aaa.replace("class","")
-            aaa=aaa.replace("void","")
-            aaa=aaa.replace("{","")
-            aaa=aaa.strip()
-            print("\n--------------------\n")
+            f1=open("/tmp/output.txt","w")
+            f1.write(u)
+            f1.close()
+            os.system("rasm2 -a java -B -f /tmp/output.txt -o output.bins")
+            f1=open("output.bins","br")
+            i=f1.read()
+            f1.close()
+            f1=open("output.bin","ba")
+            f1.write(i)
+            f1.close()
+
+            
+            u="nop\n"
+            steps=steps+1
+            if steps==5:
+                exit(0)
         g=aaa.find(": ")
         if g>-1:
-           
+            
             aaa=aaa[g+2:]
-            print(aaa)
+            u=u+aaa+"\n"
         g=aaa.find("}")
         if g>-1:
-            print("\n---------------------\nend")
+            f1=open("/tmp/output.txt","w")
+            f1.write(u)
+            f1.close()
+            os.system("rasm2 -a java -B -f /tmp/output.txt -o output.bins")
+            f1=open("output.bins","br")
+            i=f1.read()
+            f1.close()
+            f1=open("output.bin","ba")
+            f1.write(i)
+            f1.close()
+
+os.system("rasm2 -a java -b 16 -D -B -f output.bin")
+
 
